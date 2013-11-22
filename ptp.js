953c019e459680a680fc0b1311b57304f7545489
Nav Jagpal <nav@google.com>
2013-11-22 18:03:07 -0500

    Cleanup the unpacker. Add comments.

--- ptp.js
+++ ptp.js
@@ -2,29 +2,7 @@ goog.provide('ptp');
 goog.provide('ptp.PtpSession');
 goog.provide('ptp.PtpTransport');
 
-ptp.PtpUnpacker = function(raw) {
-  this.raw = raw;
-  this.offset = 0;
-};
-
-ptp.PtpUnpacker.prototype.unpack_string = function() {
-  var dataView = new DataView(this.raw);
-  var strLen = dataView.getInt8(0, true);
-  this.offset += 1;
-
-  return String.fromCharCode.apply(null,
-    new Uint16Array(this.raw.slice(
-      this.offset, this.offset + strLen * 2)));
-};
-
-PtpUnpacker.prototype.unpack_simpletype = function(
-  is_array, fmt) {
-  if (!is_array) {
-    if (fmt == '_STR') {
-      return this.unpack_string();
-    }
-  }
-};
+goog.require('ptp.Unpacker');
 
 var PtpObjectInfo = function(raw) {
   // TODO(nav): There are a bunch of fields here.
@@ -392,8 +370,8 @@ ptp.PtpSession.prototype.GetDevicePropValue = function(propertyId, isArray, fmt,
       callback(null);
       return;
     }
-    var unpacker = new PtpUnpacker(rx[1]);
-    callback(unpacker.unpack_simpletype(isArray, fmt));
+    var unpacker = new ptp.Unpacker(rx[1]);
+    callback(unpacker.unpackSimpletype(isArray, fmt));
   });
 };
 
--- unpacker.js
+++ unpacker.js
@@ -1,30 +1,56 @@
 /**
- * @fileoverview Description of this file.
+ * @fileoverview Classes related to unpacking byte data.
  */
 
-goog.provide('ptp');
 goog.provide('ptp.Unpacker');
 
-ptp.PtpUnpacker = function(raw) {
-  this.raw = raw;
-  this.offset = 0;
+/**
+ * Unpacks raw byte data from responses into the appropriate JS data types.
+ * @param {ArrayBuffer} buffer The raw byte data to unpack.
+ * @constructor
+ */
+ptp.Unpacker = function(buffer) {
+  /**
+   * Raw byte array.
+   * @type ArrayBuffer
+   * @private
+   */
+  this.buffer_ = buffer;
+
+  /**
+   * Current position within the buffer.
+   * @type Number
+   * @private
+   */
+  this.offset_ = 0;
 };
 
-ptp.PtpUnpacker.prototype.unpack_string = function() {
-  var dataView = new DataView(this.raw);
+/**
+ * Unpacks a String from the current offset.
+ * @return {String} The unpacked string.
+ * @private
+ */
+ptp.Unpacker.prototype.unpackString_ = function() {
+  var dataView = new DataView(this.buffer_);
   var strLen = dataView.getInt8(0, true);
-  this.offset += 1;
+  this.offset_ += 1;
 
   return String.fromCharCode.apply(null,
-    new Uint16Array(this.raw.slice(
-      this.offset, this.offset + strLen * 2)));
+    new Uint16Array(this.buffer_.slice(
+      this.offset_, this.offset_ + strLen * 2)));
 };
 
-PtpUnpacker.prototype.unpack_simpletype = function(
-  is_array, fmt) {
-  if (!is_array) {
+/**
+ * Convenience method to unpack common types using format strings.
+ * @param {bool} isArray Whether or not the return value is an array.
+ * @param {String} fmt Format string describing the data to unpack.
+ * @return {Object} The unpacked data.
+ */
+Unpacker.prototype.unpackSimpletype = function(
+  isArray, fmt) {
+  if (!isArray) {
     if (fmt == '_STR') {
-      return this.unpack_string();
+      return this.unpackString_();
     }
   }
 };
