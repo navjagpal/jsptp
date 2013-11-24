3c95a8d0c8f0f5faa6e6e7ae520a37ea51326692
Nav Jagpal <nav@gmail.com>
2013-11-24 11:58:37 -0500

    Add more tests.

--- tests/transport_test.js
+++ tests/transport_test.js
@@ -1,4 +1,5 @@
 goog.require('ptp.Event');
+goog.require('ptp.Request');
 goog.require('ptp.Transport');
 
 goog.require('goog.asserts');
@@ -22,6 +23,8 @@ function setUp() {
 
 var testNewSession = function() {
   assertEquals(1, transport.NewSession()); 
+  assertEquals(2, transport.NewSession());
+  assertEquals(3, transport.NewSession());
 }
 
 var testCheckEventError = function() {
@@ -39,27 +42,44 @@ var testCheckEventError = function() {
   });
   assertNull(result);
   chrome.usb.bulkTransfer.$verify();
-}
+};
 
-var getEvent = function(eventId, transactionId) {
-  var buffer = new ArrayBuffer(12);
+var getEvent = function(eventId, transactionId, params) {
+  var buffer = new ArrayBuffer(12 + params.length * 4);
   var dataView = new DataView(buffer);
-  dataView.setUint32(0, 12, true);
+  dataView.setUint32(0, 12 + params.length * 4, true);
   dataView.setUint16(4, ptp.Transport.USB_CONTAINER_EVENT, true);
   dataView.setUint16(6, eventId, true);
   dataView.setUint32(8, transactionId, true);
+  for (var i = 0; i < params.length; i++) {
+    dataView.setUint32(12 + (i * 4), params[i]);
+  } 
   return buffer;
-}
+};
+
+var getResponse = function(opcode, transactionId, params) {
+  var buffer = new ArrayBuffer(12 + params.length * 4);
+  var dataView = new DataView(buffer);
+  dataView.setUint32(0, 12 + params.length * 4, true);
+  dataView.setUint16(4, ptp.Transport.USB_CONTAINER_RESPONSE, true);
+  dataView.setUint16(6, opcode, true);
+  dataView.setUint32(8, transactionId, true);
+  for (var i = 0; i < params.length; i++) {
+    dataView.setUint32(12 + (i * 4), params[i]);
+  } 
+  return buffer;
+};
 
 var testCheckEvent = function() {
   var eventId = 20;
   var transactionId = 40;
+  var params = [100, 200];
   chrome.usb.bulkTransfer = goog.testing.createFunctionMock();
   var ignored = new goog.testing.mockmatchers.IgnoreArgument();
   chrome.usb.bulkTransfer(ignored, ignored,
     goog.testing.mockmatchers.isFunction).$does(function(
     device, transaction, callback) {
-    callback({resultCode: 0, data: getEvent(eventId, transactionId)});
+    callback({resultCode: 0, data: getEvent(eventId, transactionId, params)});
   });
   chrome.usb.bulkTransfer.$replay();
   var result = undefined;
@@ -68,28 +88,82 @@ var testCheckEvent = function() {
   });
   chrome.usb.bulkTransfer.$verify();
   goog.asserts.assertInstanceof(result, ptp.Event);
-  assertEquals(result.eventcode, eventId);
-  assertEquals(result.transactionid, transactionId);
-  goog.asserts.assertArray(result.params, []);
+  assertEquals(eventId, result.eventcode);
+  assertEquals(transactionId, result.transactionid);
+  goog.asserts.assertArray(params, result.params);
 }
 
-/**
- * Tests a problem with the underlying read. We expect a null value to be
- * returned.
- */
-var testCheckEventBadRead = function() {
+var testSendRequestError = function() {
+  var request = new ptp.Request(1, 2, 3, []);
   chrome.usb.bulkTransfer = goog.testing.createFunctionMock();
   var ignored = new goog.testing.mockmatchers.IgnoreArgument();
   chrome.usb.bulkTransfer(ignored, ignored,
     goog.testing.mockmatchers.isFunction).$does(function(
-    device, transaction, callback) {
+    device, transferInfo, callback) {
+    savedBuffer = transferInfo.data;
     callback({resultCode: 1});
   });
   chrome.usb.bulkTransfer.$replay();
   var result = undefined;
-  transport.CheckEvent(0, function(returned) {
+  transport.SendRequest(request, function(returned) {
     result = returned;
   });
   chrome.usb.bulkTransfer.$verify();
-  assertNull(result);
+  assertFalse(result);
+  // Verify the correct buffer was sent, even though there was an error
+  // in the send.
+  var dataView = new DataView(savedBuffer);
+  assertEquals(savedBuffer.byteLength, 12 );
+  assertEquals(dataView.getUint32(0, true), savedBuffer.byteLength);
+  assertEquals(dataView.getUint16(4, true), ptp.Transport.USB_CONTAINER_COMMAND);
+  assertEquals(dataView.getUint16(6, true), 1);
+  assertEquals(dataView.getUint32(8, true), 3);
+}
+
+var testSendRequest = function() {
+  var opcode = 100;
+  var sessionId = 200;
+  var transactionId = 300;
+  var params = [1, 2];
+  var request = new ptp.Request(opcode, sessionId, transactionId, params);
+  chrome.usb.bulkTransfer = goog.testing.createFunctionMock();
+  var ignored = new goog.testing.mockmatchers.IgnoreArgument();
+  var savedBuffer = null;
+  chrome.usb.bulkTransfer(ignored, ignored,
+    goog.testing.mockmatchers.isFunction).$does(function(
+    device, transferInfo, callback) {
+    savedBuffer = transferInfo.data;
+    callback({resultCode: 0});
+  });
+  chrome.usb.bulkTransfer.$replay();
+  var result = undefined;
+  transport.SendRequest(request, function(returned) {
+    result = returned;
+  });
+  chrome.usb.bulkTransfer.$verify();
+  assertTrue(result);
+  // Verify the correct buffer was sent.
+  var dataView = new DataView(savedBuffer);
+  assertEquals(12 + params.length * 4, savedBuffer.byteLength);
+  assertEquals(savedBuffer.byteLength, dataView.getUint32(0, true));
+  assertEquals(ptp.Transport.USB_CONTAINER_COMMAND, dataView.getUint16(4, true));
+  assertEquals(opcode, dataView.getUint16(6, true));
+  assertEquals(transactionId, dataView.getUint32(8, true));
+  for (var i = 0; i < params.length; i++) {
+    assertEquals(params[i], dataView.getUint32(12 + (i * 4), true));
+  }
+}
+
+var testDecodeResponse = function() {
+  var opcode = 50;
+  var sessionId = 100;
+  var transactionId = 200;
+  var params = [1, 2, 3, 4, 5];
+  var request = new ptp.Request(1, sessionId, transactionId, []);
+  var rawResponse = getResponse(opcode, transactionId, params)
+  var response = transport.DecodeResponse(request, rawResponse);
+  assertEquals(opcode, response.respcode);
+  assertEquals(sessionId, response.sessionId);
+  assertEquals(transactionId, response.transactionId);
+  goog.asserts.assertArray(params, response.params);
 }
